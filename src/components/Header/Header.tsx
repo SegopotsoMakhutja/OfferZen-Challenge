e5177bdae37a14563b0b6c4b1262f71fa06e0cc4
Segopotso Makhutja <segopotso.makhutja@sovtech.com>
2022-04-04 23:53:18 +0200

    fix(header): add aria-label for image to improve accessibility

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
             bg={"#343951"}
             borderBottom={"1px solid #272b3d"}
         >
-            <Image h={"30px"} src={'https://assets.website-files.com/6081453925c1b7567312daa7/6082922d484484bbb2c7f577_offerzen-logo-white.svg'} />
+            <Image h={"30px"} src={'https://assets.website-files.com/6081453925c1b7567312daa7/6082922d484484bbb2c7f577_offerzen-logo-white.svg'} aria-label="offerzen-logo" />
         </Flex>
     )
 }
